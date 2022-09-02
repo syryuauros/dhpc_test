648920fe0d37d7122c7776c9ed789f920ec51b37
syryuauros <sy.ryu@aurostech.com>
2022-09-02 14:33:02 +0900

    update

--- flake.nix
+++ flake.nix
@@ -16,59 +16,16 @@
         let usb-module = { pkgs, modulesPath, ...}: {
             imports = [
               (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
-              # (modulesPath + "/installer/cd-dvd/installation-cd-graphical-gnome.nix")
               (modulesPath + "/installer/scan/not-detected.nix")];};
         in nixpkgs.lib.nixosSystem {
            inherit system;
            modules = [
              usb-module
-
-             (
-               { pkgs, lib, ...} : let
-                   disk = "/dev/nvme0n1";
-                 in {
-                 # use the latest Linux kernel
-                 boot.kernelPackages = pkgs.linuxPackages_latest;
-
-                 # Needed for https://github.com/NixOS/nixpkgs/issues/58959
-                 boot.supportedFilesystems = lib.mkForce [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
-                 boot.postBootCommands = ''
-
-                  # Partitioning
-                  sudo parted ${disk} -- mklabel gpt
-                  sudo parted ${disk} -- mkpart primary 512MB -8GB
-                  sudo parted ${disk} -- mkpart primary linux-swap -8GB 100%
-                  sudo parted ${disk} -- mkpart ESP fat32 1MB 512MB
-                  sudo parted ${disk} -- set 3 esp on
-
-                  # Formatting
-                  sudo mkfs.ext4 -L root ${disk}p1
-                  sudo mkswap -L swap ${disk}p2
-                  sudo mkfs.fat -F 32 -n BOOT ${disk}p3
-
-                  # installing
-                  sudo mount ${disk}p1 /mnt
-
-                  sudo mkdir -p /mnt/boot
-                  sudo mount ${disk}p3 /mnt/boot
-                  sudo swapon ${disk}p2
-                  nixos-install --flake ${./.} .#master
-                 '';
-               }
-             )
-
-           #   ({...}: {
-           #   boot.initrd.preLVMCommands = ''
-           #     ${self.packages.${system}.run-pixiecore}/bin/run-pixiecore
-           #   '';
-           # })
-             # ({config, pkgs, ...} : {
-             #    environment.systemPackages = with pkgs; [
-             #      firefox
-             #      vim
-             #    ];
-             # })
-           ];
+             ({...}: {
+             boot.initrd.preLVMCommands = ''
+               ${self.packages.${system}.run-pixiecore}/bin/run-pixiecore
+             '';
+           })];
          });
 
       netboot = (x: x.netboot.${system})
@@ -86,7 +43,6 @@
         boot ${kernel} ${initrd} \
         --cmdline "${init} loglevel=4"\
         --debug --dhcp-no-bind --port 52647 --status-port 52647
-
       '';
     };
 
@@ -96,21 +52,6 @@
         program = "${self.packages.${system}.run-pixiecore}/bin/run-pixiecore";
       };
     };
-
-    nixosConfiguraions = {
-      master = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ({config, pkgs, ... } : {
-            imports = [
-              (import ./configuration.nix)
-              (import ./hardware-configuration.nix)
-            ];
-          })
-        ];
-      };
-    };
   };
 
 }
-
